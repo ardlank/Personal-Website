30c77b32d52351eeb3630550a80dec9e141b97b1
Ardlan Khalili <ardlankhalili@gmail.com>
2017-08-22 16:04:22 -0500

    url icon

--- Contact/index.php
+++ Contact/index.php
@@ -69,6 +69,8 @@
     <title>Ardlan Khalili | Contact Me</title>
     <link rel="stylesheet" href="../CSS/contact.css">
     <link rel="stylesheet" href="../CSS/nav.css">
+    <link rel="icon" type="image/png" href="../images/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="../images/favicon-16x16.png" sizes="16x16" />
 </head>
 
 <body>
